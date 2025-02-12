1c35786018b317e88ef4cbc9b2b3f7327027cdff
Carol-MS2001 <cintiacmartelsanchez7@gmail.com>
2025-02-12 16:48:10 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,47 @@
+//let lluviaActiva = false;
+//let intervaloLluvia;
+
+
+
+
+//function abrirCarta() {
+   /// document.getElementById("sobre").style.display = "none";
+    //ocument.getElementById("carta").style.display = "block";
+    //document.getElementById("musica").play(); // Inicia la música
+   // iniciarLluvia(); // Inicia la lluvia de corazones
+//}
+
+//function cerrarcarta() {
+  //  document.getElementById("sobre").style.display = "block";
+    //document.getElementById("carta").style.display = "none";
+    //document.getElementById("musica").pause(); // Pausa la música
+    //detenerLluvia(); // Detiene la lluvia de corazones
+//}
+
+// Función para iniciar la lluvia de corazones
+//function iniciarLluvia() {
+  //  if (lluviaActiva) return;
+   // lluviaActiva = true;
+
+   // intervaloLluvia = setInterval(() => {
+     //   let corazon = document.createElement("div");
+       // corazon.classList.add("corazon");
+       // corazon.innerHTML = "❤️";
+       // corazon.style.left = Math.random() * 100 + "vw";
+       // corazon.style.top= "-50px"
+        //corazon.style.animationDuration = Math.random() * 3 + 2 + "s";
+        //document.getElementById("corazon-coiner ").appendchild (corazon); 
+   
+
+        ///setTimeout(() => {
+           // corazon.remove();
+        //}//, 5000);
+    //}, 300);
+//}
+
+// Función para detener la lluvia de corazones
+//function detenerLluvia() {
+  //  lluviaActiva = false;
+    //clearInterval(intervaloLluvia);
+    //document.querySelectorAll(".corazon").forEach(corazon => corazon.remove());
+//}
